e1e1ef5cdb6ef03afffab30fb46547343e0a80e7
Kacper Pietrasik <kpt@touk.pl>
2012-07-10 12:15:49 +0200

    Remove test in ProjectUpdateJobSpec.

--- test/unit/codereview/ProjectUpdateJobSpec.groovy
+++ test/unit/codereview/ProjectUpdateJobSpec.groovy
@@ -1,27 +1,29 @@
-package codereview
+//package codereview
+//
 
-import grails.test.mixin.Mock
-import spock.lang.Specification
-import grails.test.mixin.TestFor
-
-@TestFor(ProjectUpdateJob)
-@Mock(Changeset)
-class ProjectUpdateJobSpec extends Specification {
-
-    def "Should do something"() {
-        given:
-
-        ProjectUpdateJob projectUpdateJobMock = Mock()
-        projectUpdateJobMock.execute() >> { new Changeset("flald", "agj", new Date()).save() }
-
-
-        when:
-        projectUpdateJobMock.execute()
-
-        then:
-        Changeset.count() != 0
-        1 == 2
-    }
-
-
-}
+//FIXME: new test
+//import grails.test.mixin.Mock
+//import spock.lang.Specification
+//import grails.test.mixin.TestFor
+//
+//@TestFor(ProjectUpdateJob)
+//@Mock(Changeset)
+//class ProjectUpdateJobSpec extends Specification {
+//
+//    def "Should do something"() {
+//        given:
+//
+//        ProjectUpdateJob projectUpdateJobMock = Mock()
+//        projectUpdateJobMock.execute() >> { new Changeset("flald", "agj", new Date()).save() }
+//
+//
+//        when:
+//        projectUpdateJobMock.execute()
+//
+//        then:
+//        Changeset.count() != 0
+//        1 == 2
+//    }
+//
+//
+//}
