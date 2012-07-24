c2635a406f7f85bc4a94493724ef1ee66546ee63
Kacper Pietrasik <kpt@touk.pl>
2012-07-24 16:47:24 +0200

    almost working scroll-to-bottom paging ;)

--- grails-app/controllers/codereview/ChangesetController.groovy
+++ grails-app/controllers/codereview/ChangesetController.groovy
@@ -8,20 +8,19 @@ class ChangesetController {
     ScmAccessService scmAccessService
 
     def index() {
-
     }
 
     /**
      * TODO helper method for development phase
      */
     def initialCheckOut() {
-        log.info("Checking out project.")
+        log.info('Checking out project.')
         scmAccessService.checkoutProject(Fixture.PROJECT_REPOSITORY_URL)
-        redirect(views: "/index", params: params)
+        redirect(views: '/index', params: params)
     }
 
     def getLastChangesets = {
-        render Changeset.list(max: 21, sort: "date", order: "desc") as JSON
+        render Changeset.list(max: 21, sort: 'date', order: 'desc') as JSON
     }
 
     def getFileNamesForChangeset = {      //needs id of changeset
@@ -30,24 +29,18 @@ class ChangesetController {
         render files as JSON
     }
 
-    def getNextTenChangesets = {
-        def id = params.id
-        def myOffset = id.toInteger() * 10 + 1
-        render Changeset.list(max: 10, sort: "date", order: "desc", offset: myOffset) as JSON
-    }
-
-    def nextTenChangesets = {       //TODO:refactor name getAnotherTenChangesets
-        def offset = params.id     //TODO Why? It's actually not getting, but rendering - check view, javascript involved
-        [offset: offset]
-    }
     def getChangeset = {
         def id = params.id
         def changeset = Changeset.findById(params.id)
         def changesetList = [changeset]
         render changesetList as JSON
     }
-    def codeViewing = {
 
+    def getNextFewChangesetsOlderThan = { String revisionId ->
+        def nextFewChangesets = Changeset.where {
+            date < property(date).of { identifier == revisionId }
+        }.list(max: 10, sort: 'date', order: 'desc')
+        render nextFewChangesets as JSON
     }
 }
 
