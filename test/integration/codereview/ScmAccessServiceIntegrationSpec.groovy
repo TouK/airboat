1f072a2a027162523fb2b3c27ad035867fcf8a47
Justyna Ilczuk <jil@touk.pl>
2012-07-26 15:33:51 +0200

    first steps in adding commiters and users

--- test/integration/codereview/ScmAccessServiceIntegrationSpec.groovy
+++ test/integration/codereview/ScmAccessServiceIntegrationSpec.groovy
@@ -17,27 +17,27 @@ class ScmAccessServiceIntegrationSpec extends IntegrationSpec {
 
     def "should fetch and save changesets in db"() {    //TODO it's inconsistent with our naming convention
         given:
-            def (gitScmUrl, changesetId, commitComment, changesetAuthor)  = [Fixture.PROJECT_REPOSITORY_URL, "id", "comment", "agj@touk.pl"]
+        def (gitScmUrl, changesetId, commitComment, changesetAuthor) = [Fixture.PROJECT_REPOSITORY_URL, "id", "comment", "agj@touk.pl"]
 
-            def cs = new ChangeSet(new Date(), commitComment, changesetAuthor, null)
-            cs.setRevision(changesetId)
-            GitRepositoryService gitRepositoryService = Mock()
-            1 * gitRepositoryService.getAllChangeSets(Fixture.PROJECT_REPOSITORY_URL) >> [ cs ]
+        def cs = new ChangeSet(new Date(), commitComment, changesetAuthor, null)
+        cs.setRevision(changesetId)
+        GitRepositoryService gitRepositoryService = Mock()
+        1 * gitRepositoryService.getAllChangeSets(Fixture.PROJECT_REPOSITORY_URL) >> [cs]
 
-            scmAccessService.gitRepositoryService = gitRepositoryService
+        scmAccessService.gitRepositoryService = gitRepositoryService
 
         when:
-            scmAccessService.importAllChangesets(gitScmUrl)
+        scmAccessService.importAllChangesets(gitScmUrl)
 
         then:
-            Changeset.count() == 1
-            Changeset.findAllByIdentifierAndCommitComment(changesetId, commitComment).size() == 1
+        Changeset.count() == 1
+        Changeset.findAllByIdentifierAndCommitComment(changesetId, commitComment).size() == 1
     }
 
     //TODO this testing is incomplete, because service has got many methods and they're aren't tested anywhere - More tests!
 
     //FIXME test for sort order of changesets
-    def "should convert git ChangeSet to ChangeSet and add commiter" () {
+    def "should convert git ChangeSet to ChangeSet and add commiter"() {
         given:
         def gitChangeSet = new ChangeSet(new Date(), "Refactoring", "jil <jil@touk.pl>", null)
 
@@ -51,7 +51,7 @@ class ScmAccessServiceIntegrationSpec extends IntegrationSpec {
 
     }
 
-    def "should save Changeset to db and add commiter if it is not yet in db" () {
+    def "should save Changeset to db and add commiter if it is not yet in db"() {
         given:
         def cvsCommiterId = "jil <jil@touk.pl>"
         def gitChangeSet = new ChangeSet(new Date(), "Refactoring", cvsCommiterId, null)
@@ -64,12 +64,12 @@ class ScmAccessServiceIntegrationSpec extends IntegrationSpec {
         def commiter = Commiter.findByCvsCommiterId(cvsCommiterId)
         commiter != null
         Changeset.findByCommiter(commiter) != null
-        def changesetFromDB =  Changeset.findByCommiter(changeset.commiter)
-        Commiter.findByCvsCommiterId(changesetFromDB.getEmail())  == 1
+        def changesetFromDB = Changeset.findByCommiter(changeset.commiter)
+        Commiter.findByCvsCommiterId(changesetFromDB.getEmail()) == 1
 
     }
 
-    def "should save Changeset to db and not add a new commiter to db if it already exists"  () {
+    def "should save Changeset to db and not add a new commiter to db if it already exists"() {
         given:
         new Commiter("Jil <jil@touk.pl>").save()
 
@@ -78,7 +78,7 @@ class ScmAccessServiceIntegrationSpec extends IntegrationSpec {
         def commiter = new Commiter("Jil <jil@touk.pl>")
         commiter.addToChangesets(changeset)
         scmAccessService.saveChangeset(changeset)
-        def changesetFromDB =  Changeset.findByAuthor("jil <jil@touk.pl>")
+        def changesetFromDB = Changeset.findByAuthor("jil <jil@touk.pl>")
         def commiterFromDb = Commiter.findByEmail("jil@touk.pl")
         Commiter.findByChangesets
 
