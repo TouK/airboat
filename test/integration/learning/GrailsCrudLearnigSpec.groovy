6cd9a4c45d85fa64be04e16fd0c7bf76cfdbc390
Justyna Ilczuk <jil@touk.pl>
2012-07-27 14:36:01 +0200

    Better implementation of test in GitRepositoryServiceIntegrationSpec

---
+++ test/integration/learning/GrailsCrudLearnigSpec.groovy
@@ -0,0 +1,20 @@
+package learning
+
+import spock.lang.Specification
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: touk
+ * Date: 27.07.12
+ * Time: 12:30
+ * To change this template use File | Settings | File Templates.
+ */
+class GrailsCrudLearnigSpec extends Specification {
+    def "How exactly saving works when there are relationships between grails domain classes"(){
+
+    }
+    def "How to save when belongs to" () {
+
+    }
+
+}
