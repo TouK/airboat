4a977064814d8097801c8b837a42a6086c981d1e
Artur Gajowy <agj@touk.pl>
2012-08-02 08:59:53 +0200

    fixed a stupid bug, thank you dynamic typing...

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,8 +1,11 @@
 import codereview.Changeset
 import grails.converters.JSON
 
+import static codereview.ScmAccessService.getEmail
+
 class BootStrap {
 
+    //FIXME add a bootstrap test, errors here are too frequent...
     def init = { servletContext ->
 
         JSON.registerObjectMarshaller(Changeset) {
@@ -10,7 +13,7 @@ class BootStrap {
             returnMap['identifier'] = it.identifier
             returnMap['author'] = it.commiter.cvsCommiterId
             returnMap['date'] = it.date
-            returnMap['email'] = it.commiter.getEmail()
+            returnMap['email'] = getEmail(it.commiter.cvsCommiterId) //FIXME use changeset.committer.user.email or equivalent ASAP
             returnMap['commitComment'] = it.commitComment
             returnMap['id'] = it.id
             returnMap['commentsCount'] = it.commentsCount()
