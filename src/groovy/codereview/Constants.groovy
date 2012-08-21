6c76bedd81c90ab4464e62ed001c89140dfa4f73
Artur Gajowy <agj@touk.pl>
2012-08-21 16:26:56 +0200

    fixing/removing some defective tests. Heads will roll.

--- src/groovy/codereview/Constants.groovy
+++ src/groovy/codereview/Constants.groovy
@@ -5,4 +5,5 @@ class Constants {
     public static final String PROJECT_CYCLONE_REPOSITORY_URL = 'git://git.touk.pl/qrios/cyclone.git'
 
     public static final String TOUK_GIT_REPOS_SERVER = 'git.touk.pl'
+    public static final String CODEREVIEW_WORKING_DIRECTORY_PROPERTY = 'codereview.workingDirectory'
 }
--- test/unit/codereview/InfrastructureServiceSpec.groovy
+++ test/unit/codereview/InfrastructureServiceSpec.groovy
@@ -4,15 +4,38 @@ import grails.test.mixin.Mock
 import spock.lang.Specification
 import testFixture.Fixture
 
+import static codereview.Constants.CODEREVIEW_WORKING_DIRECTORY_PROPERTY
+
 @Mock(Project)
 class InfrastructureServiceSpec extends Specification {
 
-    def 'temporary dir path should be returned for base directory name'() {
+    String workingDirectory
+
+    def setup() {
+        workingDirectory = System.getProperty(CODEREVIEW_WORKING_DIRECTORY_PROPERTY)
+    }
+
+    def cleanup() {
+        System.setProperty(CODEREVIEW_WORKING_DIRECTORY_PROPERTY, workingDirectory)
+    }
+
+    def 'should use codereview.workingDirectory parameter to determine base directory name'() {
         when:
+        def overridenDirectory = "wherever-you-want"
+        System.setProperty(CODEREVIEW_WORKING_DIRECTORY_PROPERTY, overridenDirectory)
         def name = new InfrastructureService().getBaseDirectoryName()
 
         then:
-        name == '/tmp/codereview-work' //FIXME os-dependent, environment-dependent
+        name == overridenDirectory
+    }
+
+    def 'temporary dir path should be returned for base directory name if not configured otherwise'() {
+        when:
+        System.clearProperty(CODEREVIEW_WORKING_DIRECTORY_PROPERTY)
+        def name = new InfrastructureService().getBaseDirectoryName()
+
+        then:
+        name == new File(System.getProperty('java.io.tmpdir'), 'codereview-work').getAbsolutePath()
     }
 
     def 'getDirectoryNameForTheProject() should return project name'() {
